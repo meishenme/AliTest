f71c435e423ebd4de0f8c7d817caa308c67043e2
zx <meishenmezzz@163.com>
2014-03-28 16:05:40 +0800

    zx commit

--- AliTest/src/com/zx/Ali.java
+++ AliTest/src/com/zx/Ali.java
@@ -1,7 +1,57 @@
 package com.zx;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.StringTokenizer;
+
 public class Ali {
 	public static void main(String[] args) {
-		System.out.println("Test");
+		try {
+			File csv = new File("C:\\Users\\Jim\\Desktop\\t_alibaba_data.csv");
+			File csvNew = new File("C:\\Users\\Jim\\Desktop\\t_alibaba_data_1.txt");
+			
+			BufferedReader br = new BufferedReader(new FileReader(csv));
+			BufferedWriter bw = new BufferedWriter(new FileWriter(csvNew,false));
+			
+			br.readLine();//读标题行
+			
+			String line = "";
+			int column;
+			
+			while((line = br.readLine()) != null){
+				
+				column = 0;
+				StringTokenizer st = new StringTokenizer(line, ",");
+				
+				while(st.hasMoreTokens()){
+					column++;
+					if(column == 4){
+						StringTokenizer stmonth = new StringTokenizer(st.nextToken(), "月");
+						StringBuilder date = new StringBuilder(stmonth.nextToken()+".");
+	
+						StringTokenizer stday = new StringTokenizer(stmonth.nextToken(),"日");
+						date.append(stday.nextToken());
+						System.out.print(date);
+						
+						bw.write(date.toString());
+						
+					}else{
+						String newline = st.nextToken();
+						System.out.print(newline + "\t");
+						
+						bw.write(newline + ",");
+					}
+				}
+				System.out.println();
+				bw.newLine();
+			}
+			bw.close();
+			br.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 }
